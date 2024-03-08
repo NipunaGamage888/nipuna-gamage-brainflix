f257a4c6abb77d1b8560865055b4b236ee36d313
unknown <nipunagamage888@gmail.com>
2024-03-07 22:06:27 -0500

    Finished the button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Button(props) {
+  return (
+    <button className='button'>
+        <img className='button__image' src={props.image} alt='small icon of uploading'/>
+        <p className='button__name'>{props.buttonName}</p>
+    </button>
+  )
+}
+
+export default Button
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import logo1 from '../../assets/logo/BrainFlix-logo.svg'
+import profile from '../../assets/images/Mohan-muruge.jpg'
+import './header.css'
 
 
 const Header = () => {
   return (
-    <header>
-      <img src={logo1} alt='Logo of the page'/>
-        <div>
-            <input type='search' placeholder='Search'/>
+    <header className='header'>
+      <img className='header__logo' src={logo1} alt='Logo of the page'/>
+        <div className='header__input-holder'>
+            <input className='header__input' type='search' placeholder='Search'/>
+            <img className='header__profile-image' src={profile}/>
         </div>
     </header>
   )
