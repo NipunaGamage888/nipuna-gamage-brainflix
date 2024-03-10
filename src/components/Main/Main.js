3dfec2961a45a6ef73f98c53632af4ca551c74a4
unknown <nipunagamage888@gmail.com>
2024-03-10 10:37:58 -0400

    Completed the html of the next video section and got the filtered videos working

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,18 +5,27 @@ import videoDetails from '../../assets/data/video-details.json'
 import videos from '../../assets/data/videos.json'
 import Comment from '../Comment/Comment.js'
 import CommentInfo from '../CommentInfo/CommentInfo.js'
-
+import NextVideo from '../NextVideo/NextVideo.js'
+// {
+//   "id": "84e96018-4022-434e-80bf-000ce4cd12b8",
+//   "title": "The Future of Artificial Intelligence",
+//   "channel": "Aiden Thompson",
+//   "image": "https://unit-3-project-api-0a5620414506.herokuapp.com/images/image0.jpg"
+// },
 
 const Main = () => {
 
-    const[comments,setComments]=useState(videos)
+    const[video,setVideos]=useState(videos)
     const [selectedVideo, setselectedVideo]=useState(videoDetails[0])
+
+    const filteredVideo = videos.filter((video)=> video.id !==selectedVideo.id)
   return (
     <>
         <Header/>
         <VideoPlaying videoDetails={selectedVideo}/>
         <Comment/>
         <CommentInfo videoDetails={selectedVideo}/>
+        <NextVideo videos={filteredVideo}/>
     </>
   )
 }
---
+++ src/components/NextVideo/NextVideo.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './nextvideo.css'
+// {
+//     "id": "84e96018-4022-434e-80bf-000ce4cd12b8",
+//     "title": "The Future of Artificial Intelligence",
+//     "channel": "Aiden Thompson",
+//     "image": "https://unit-3-project-api-0a5620414506.herokuapp.com/images/image0.jpg"
+//   },
+function NextVideo({videos}) {
+  return (
+    <section className='videos'>
+        <h3 className='videos__heading'>Next Videos</h3>
+        <div className='videos__map'> 
+            {videos.map((video)=>{
+                return(
+                    <button key={video.id} className='videos__detail'>
+                        <img className='videos__image' src={video.image}/>
+                        <div className='videos__info'>
+                            <p className='videos__title'>{video.title}</p>
+                            <p className='videos__channel'>{video.channel}</p>
+                        </div>
+                    </button>
+                )
+            })}
+        </div>
+    </section>
+  )
+}
+
+export default NextVideo
